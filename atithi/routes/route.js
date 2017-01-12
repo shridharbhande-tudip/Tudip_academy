2c66c07193d5f242c4184c06672e2a80b8c287b8
shridhar bhande <shridhar.bhande@tudip.nl>
2017-01-13 01:47:24 +0530

    New feature: #338314 Title: Developing atithi application Details:
    Created following APIs--
    	-Sign up
    	-Login
    	-Add visitor
    	-View visitor
    	-update
    	-delete
    	-Add helping files

---
+++ atithi/routes/route.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+var app = express.Router();
+
+var auth = require('../controllers/auth');
+var visitors = require('../controllers/visitor');
+
+// User's routes
+app.post('/register', auth.register);
+app.post('/login', auth.login);
+
+//Visitor's routes
+app.post('/add', visitors.add);
+app.delete('/delete/:id', visitors.delete);
+app.post('/update/:id', visitors.update);
+app.get('/find_id/:id', visitors.find_id);
+app.get('/findAll/', visitors.findAll);
+
+module.exports = app;
